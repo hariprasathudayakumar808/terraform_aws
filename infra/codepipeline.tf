05912815424301c23a39c61fffc743e04f61e1f6
hariprasathudayakumar808 <hariprasath808@gmail.com>
2025-03-30 17:31:11 +0200

    modified build output

--- infra/codepipeline.tf
+++ infra/codepipeline.tf
@@ -39,7 +39,7 @@ resource "aws_codebuild_project" "flask_gpt_deploy" {
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = "pipeline/buildspec-deploy.yml"
+    buildspec = "buildspec-deploy.yml"
   }
 }
 
